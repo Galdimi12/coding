a8ac1f68032d8567e0ea17b9f04d98b22ab8884b
TEAM-DEPISCH\galdim.hajredinaj <galdimhajredinaj@gmail.com>
2022-03-28 19:00:23 +0200

    Exercise 5.2

--- src/main/java/Shembulli94.java
+++ src/main/java/Shembulli94.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class Shembulli94 {
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
         int number1;
         int number2;
-        int result;
-        Scanner input = new Scanner(System.in);
+        int result = number1 * number2;
+
+        number1 = input.nextInt();
+        number2 = input.nextInt();
         System.out.println("Enter number1: " + number1);
         System.out.println("Enter number2: "+ number2);
 
-        While (number1 )
+        While(number1) {
+            System.out.println(result);
+
+
+        }
     }
 
 }
---
+++ src/main/java/Ushtrime52.java
@@ -0,0 +1,7 @@
+public class Ushtrime52 {
+
+    public static void main(String[] args) {
+
+        System.out.println("print");
+    }
+}
